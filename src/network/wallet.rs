5f01fea4812c36b40835d3ba503dcb6a94ba9619
Xudong Huang <huangxu008@hotmail.com>
2018-12-12 15:03:11 +0800

    :pencil: sdg client wait handshake init done

--- src/network/wallet.rs
+++ src/network/wallet.rs
@@ -9,61 +9,63 @@ use joint::Joint;
 use light;
 use may::coroutine;
 use may::net::TcpStream;
+use may::sync::Semphore;
 use serde_json::{self, Value};
 use tungstenite::client::client;
 use tungstenite::handshake::client::Request;
 use tungstenite::protocol::Role;
 use url::Url;
 
-#[derive(Default)]
-pub struct WalletData {}
+//---------------------------------------------------------------------------------------
+// WalletData
+//---------------------------------------------------------------------------------------
+pub struct WalletData {
+    init_done: Semphore,
+}
 
-pub type WalletConn = WsConnection<WalletData>;
+impl WalletData {
+    fn wait_init_done(&self) {
+        self.init_done.wait();
+    }
 
-fn init_connection(ws: &Arc<WalletConn>) -> Result<()> {
-    use rand::{thread_rng, Rng};
+    fn trigger_init_done(&self) {
+        self.init_done.post();
+    }
+}
 
-    ws.send_version()?;
+impl Default for WalletData {
+    fn default() -> Self {
+        WalletData {
+            init_done: Semphore::new(0),
+        }
+    }
+}
+impl Server<WalletData> for WalletData {
+    fn on_message(ws: Arc<WalletConn>, subject: String, body: Value) -> Result<()> {
+        match subject.as_str() {
+            "version" => ws.on_version(body)?,
+            "hub/challenge" => ws.on_hub_challenge(body)?,
+            subject => error!("on_message unknown subject: {}", subject),
+        }
+        Ok(())
+    }
 
-    let mut rng = thread_rng();
-    let n: u64 = rng.gen_range(0, 1000);
-    let ws_c = Arc::downgrade(ws);
-
-    // start the heartbeat timer for each connection
-    go!(move || loop {
-        coroutine::sleep(Duration::from_millis(3000 + n));
-        let ws = match ws_c.upgrade() {
-            Some(ws) => ws,
-            None => return,
+    fn on_request(ws: Arc<WalletConn>, command: String, params: Value) -> Result<Value> {
+        let response = match command.as_str() {
+            "heartbeat" => ws.on_heartbeat(params)?,
+            "subscribe" => ws.on_subscribe(params)?,
+            command => bail!("on_request unknown command: {}", command),
         };
-        if ws.get_last_recv_tm().elapsed() < Duration::from_secs(5) {
-            continue;
-        }
-        // heartbeat failed so just close the connection
-        let rsp = ws.send_heartbeat();
-        if rsp.is_err() {
-            error!("heartbeat err= {}", rsp.unwrap_err());
-        }
-    });
+        Ok(response)
+    }
 
-    Ok(())
+    fn close(_ws: Arc<WalletConn>) {}
 }
 
-pub fn create_outbound_conn<A: ToSocketAddrs>(address: A) -> Result<Arc<WalletConn>> {
-    let stream = TcpStream::connect(address)?;
-    let peer = match stream.peer_addr() {
-        Ok(addr) => addr.to_string(),
-        Err(_) => "unknown peer".to_owned(),
-    };
-    let url = Url::parse("wss://localhost/")?;
-    let req = Request::from(url);
-    let (conn, _) = client(req, stream)?;
-
-    let ws = WsConnection::new(conn, WalletData::default(), peer, Role::Client)?;
-
-    init_connection(&ws)?;
-    Ok(ws)
-}
+//---------------------------------------------------------------------------------------
+// WalletConn
+//---------------------------------------------------------------------------------------
+pub type WalletConn = WsConnection<WalletData>;
 
 impl WalletConn {
     fn send_version(&self) -> Result<()> {
@@ -158,11 +160,9 @@ impl WalletConn {
     }
 
     fn on_hub_challenge(&self, param: Value) -> Result<()> {
-        // TODO: add special wallet logic here
-        // this is hub, we do nothing here
-        // only wallet would save the challenge
-        // for next login and match
+        // until now the connect hand shake done
         info!("peer is a hub, challenge = {}", param);
+        self.get_data().trigger_init_done();
         Ok(())
     }
 
@@ -175,24 +175,52 @@ impl WalletConn {
     }
 }
 
-impl Server<WalletData> for WalletData {
-    fn on_message(ws: Arc<WalletConn>, subject: String, body: Value) -> Result<()> {
-        match subject.as_str() {
-            "version" => ws.on_version(body)?,
-            "hub/challenge" => ws.on_hub_challenge(body)?,
-            subject => error!("on_message unknown subject: {}", subject),
-        }
-        Ok(())
-    }
+//---------------------------------------------------------------------------------------
+// Global Functions
+//--------------------------------------------------------------------------------------
+fn init_connection(ws: &Arc<WalletConn>) -> Result<()> {
+    use rand::{thread_rng, Rng};
 
-    fn on_request(ws: Arc<WalletConn>, command: String, params: Value) -> Result<Value> {
-        let response = match command.as_str() {
-            "heartbeat" => ws.on_heartbeat(params)?,
-            "subscribe" => ws.on_subscribe(params)?,
-            command => bail!("on_request unknown command: {}", command),
+    ws.send_version()?;
+
+    let mut rng = thread_rng();
+    let n: u64 = rng.gen_range(0, 1000);
+    let ws_c = Arc::downgrade(ws);
+
+    // start the heartbeat timer for each connection
+    go!(move || loop {
+        coroutine::sleep(Duration::from_millis(3000 + n));
+        let ws = match ws_c.upgrade() {
+            Some(ws) => ws,
+            None => return,
         };
-        Ok(response)
-    }
+        if ws.get_last_recv_tm().elapsed() < Duration::from_secs(5) {
+            continue;
+        }
+        // heartbeat failed so just close the connection
+        let rsp = ws.send_heartbeat();
+        if rsp.is_err() {
+            error!("heartbeat err= {}", rsp.unwrap_err());
+        }
+    });
 
-    fn close(_ws: Arc<WalletConn>) {}
+    // wait for hub/challenge done for hand shake
+    ws.get_data().wait_init_done();
+    Ok(())
+}
+
+pub fn create_outbound_conn<A: ToSocketAddrs>(address: A) -> Result<Arc<WalletConn>> {
+    let stream = TcpStream::connect(address)?;
+    let peer = match stream.peer_addr() {
+        Ok(addr) => addr.to_string(),
+        Err(_) => "unknown peer".to_owned(),
+    };
+    let url = Url::parse("wss://localhost/")?;
+    let req = Request::from(url);
+    let (conn, _) = client(req, stream)?;
+
+    let ws = WsConnection::new(conn, WalletData::default(), peer, Role::Client)?;
+
+    init_connection(&ws)?;
+    Ok(ws)
 }
