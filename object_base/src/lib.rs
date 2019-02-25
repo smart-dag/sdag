26ff61613dbc6b9d9d331efb01cde8c6e8152a6d
Xudong Huang <huangxu008@hotmail.com>
2019-02-25 14:13:48 +0800

    :pencil: remove unused dependent crates

--- object_base/src/lib.rs
+++ object_base/src/lib.rs
@@ -1,16 +1,16 @@
+extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate failure;
-extern crate serde_json;
+
 #[macro_use]
 extern crate lazy_static;
 
 extern crate base32;
 extern crate base64;
 extern crate bit_vec;
+extern crate failure;
 extern crate rand;
 extern crate ripemd160;
-extern crate serde;
 extern crate sha2;
 
 pub mod obj_ser;
